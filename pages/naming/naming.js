295f0b5222ad3add803969df65143d762dfb9317
joy917 <296351685@qq.com>
2025-06-10 00:32:05 +0800

    增加按辈分起名模式；修改起名、重命名、解析页样式

--- pages/naming/naming.js
+++ pages/naming/naming.js
@@ -5,7 +5,8 @@ Page({
     date: '',
     time: '',
     wordCount: 2,
-    results: []
+    results: [],
+    generationChar: ''
   },
   onModeSelect(e) {
     this.setData({ mode: e.currentTarget.dataset.mode });
@@ -22,18 +23,32 @@ Page({
   onCountSelect(e) {
     this.setData({ wordCount: Number(e.currentTarget.dataset.count) });
   },
+  onGenerationCharInput(e) {
+    this.setData({ generationChar: e.detail.value });
+  },
   onGenerate() {
-    // 这里可接入真实算法或API
     wx.showLoading({ title: '生成中...' });
     setTimeout(() => {
       wx.hideLoading();
-      const results = [
-        { name: '梓涵', reason: '寓意美好，朗朗上口' },
-        { name: '子墨', reason: '取自古诗，文雅大气' },
-        { name: '思远', reason: '志向远大，易于记忆' },
-        { name: '若溪', reason: '如溪水般温柔' },
-        { name: '宇轩', reason: '气宇轩昂，阳光自信' }
-      ];
+      let results = [];
+      if (this.data.mode === 'generation' && this.data.generationChar) {
+        // 按辈分起名，固定第一个字
+        results = [
+          { name: this.data.generationChar + '涵', reason: '辈分字+' + '寓意美好，朗朗上口' },
+          { name: this.data.generationChar + '墨', reason: '辈分字+' + '取自古诗，文雅大气' },
+          { name: this.data.generationChar + '远', reason: '辈分字+' + '志向远大，易于记忆' },
+          { name: this.data.generationChar + '溪', reason: '辈分字+' + '如溪水般温柔' },
+          { name: this.data.generationChar + '轩', reason: '辈分字+' + '气宇轩昂，阳光自信' }
+        ];
+      } else {
+        results = [
+          { name: '梓涵', reason: '寓意美好，朗朗上口' },
+          { name: '子墨', reason: '取自古诗，文雅大气' },
+          { name: '思远', reason: '志向远大，易于记忆' },
+          { name: '若溪', reason: '如溪水般温柔' },
+          { name: '宇轩', reason: '气宇轩昂，阳光自信' }
+        ];
+      }
       wx.navigateTo({
         url: '/pages/naming/result?results=' + encodeURIComponent(JSON.stringify(results))
       });
